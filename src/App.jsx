944224a6b5958274af2b8e44eb66f47155199fde
SalcRusso333 <tssmirsalruso@gmail.com>
2024-04-22 14:49:17 -0700

    Cree dos css y agrege mas lineas de codigo al App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,25 @@
 import { useState } from 'react'
 import SearchBar from './componentes/SearchBar'
+import ImageList from './componentes/imageList'
+import searchImage from './api'
 import './App.css'
+
 
 function App() {
   const [images, setImagas] = useState([])
 
   const handleSubmit = async(term) => {
     console.log('ustate esta buscando con: ', term)
+
+    console.log('coco')
+    console.log(result)
   }
 
   return (
     <div>
   <h1>App</h1>
        <SearchBar onSubmit={handleSubmit} />
+       <ImageList images={images} />
     </div>
 
   )
--- src/componentes/imageList.jsx
+++ src/componentes/imageList.jsx
@@ -1,12 +1,12 @@
 import imageShow from './imageShow'
-
+import './ImageList.css'
 function ImageList ({images}){
     const renderImage = images.map((image) => {
         return <imageShow key={image.id} image={image} />
     })
     
     return(
-    <div>
+    <div className='image-list'>
         {renderImage}
     </div>
    )
